20b3981bcb44e32ba4ccfe1ad0e58e2b6bbf0710
lithiumix <lithiumix@yahoo.com>
2026-09-20 16:13:23 +0800

    Update AMI filter value in main.tf

--- main.tf
+++ main.tf
@@ -3,7 +3,7 @@ data "aws_ami" "app_ami" {
 
   filter {
     name   = "name"
-    values = ["aws-elasticbeanstalk-amzn-*.64bit-eb_tomcat11corretto17_amazon_linux_2023-hvm-*.770Z"]
+    values = ["aws-elasticbeanstalk-amzn-2023.12.20260831.64bit-eb_tomcat11corretto17_amazon_linux_2023-hvm-2026-09-09T01-41-48.770Z"]
   }
 
   filter {
